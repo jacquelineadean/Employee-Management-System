438aa4bb36b12870953053d65f1bc2db19ee9b65
Jacqueline Dean <jacquelineadean@gmail.com>
2020-07-20 15:20:46 -0500

    Updated id in schema.sql

--- schema.sql
+++ schema.sql
@@ -1,12 +1,12 @@
-DROP DATABASE IF EXISTS CMS_DB;
-CREATE database CMS_DB;
+DROP DATABASE IF EXISTS employeeTracker_DB;
+CREATE database employeeTracker_DB;
 
-USE CMS_DB;
+USE employeeTracker_DB;
 
 -- department table 
 CREATE TABLE department (
-  -- id  
-  id INT,
+  -- id will automatically increment  
+  id INT NOT NULL AUTO_INCREMENT,
   -- department name
   name VARCHAR(30),
   -- set id as the primary key
@@ -15,8 +15,8 @@ CREATE TABLE department (
 
 -- role table
 CREATE TABLE role (
-  -- id
-  id INT,
+  -- id will automatically increment 
+  id INT NOT NULL AUTO_INCREMENT,
   -- title to hold role title
   title VARCHAR(30),
   -- salary 
@@ -29,8 +29,8 @@ CREATE TABLE role (
 
 -- employee table
 CREATE TABLE employee (
-  -- id
-  id INT,
+  -- id will automatically increment 
+  id INT NOT NULL AUTO_INCREMENT,
   -- employee first name
   first_name VARCHAR(30),
   -- employee last name 
